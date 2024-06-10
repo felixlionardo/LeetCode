a11f68e40f6fb51d9545edf2e1e6098a8237452e
felixlionardo <70541493+felixlionardo@users.noreply.github.com>
2024-06-09 21:57:17 -0700

    Time: 67 ms (43.12%), Space: 17.8 MB (36.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numMap = {}
-        n = len(nums)
 
-        for i in range(n):
-            complement = target - nums[i]
+        for x in range(0,len(nums)):
+            complement = target - nums[x]
             if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
-
-        return []  # No solution found
+                return [numMap[complement], x]
+            numMap[nums[x]] = x
+        return []
